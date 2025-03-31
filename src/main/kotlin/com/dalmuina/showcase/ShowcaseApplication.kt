a630e13845033e86f7698206c29b5b90d5e0d540
Diego <dalmuina@gmail.com>
2025-03-31 19:41:34 +0200

    First Commit

---
+++ src/main/kotlin/com/dalmuina/showcase/ShowcaseApplication.kt
@@ -0,0 +1,11 @@
+package com.dalmuina.showcase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShowcaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShowcaseApplication>(*args)
+}
